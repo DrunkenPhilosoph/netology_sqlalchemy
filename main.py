4d951afdd959ae83c95159df03bc082d088396f7
Mushinskiy E <drunken.philosopher.icloud@gmail.com>
2024-01-17 18:51:15 +0300

    new function add_data

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import sessionmaker
 import models
-from models import create_tables, Publicist, Book
+from models import create_tables, Publicist, Book, Shop, Stock, Sale
 import json
 
-DSN = 'postgresql://postgres:1234@localhos:5433?charset=utf-8'
+DSN = 'postgresql://postgres:1234@localhost:5432'
 engine = sq.create_engine(DSN)
 Session = sessionmaker(engine)
 session = Session()
 models.create_tables(engine)
 
-with open("tests_data.json", encoding='utf-8') as file:
-    json_data = json.load(file)
-    print(json_data)
-# data = json.load("test_data.json", encoding="unicode", errors='ignore')
-# print(data)
+def add_data(path):
+    with open(path, encoding='utf-8') as file:
+        json_data = json.load(file)
+        for item in json_data:
+            print(item['model'])
+            if item['model'] == 'publisher':
+                publicists = Publicist(id=item['pk'], name=item['fields']['name'])
+                session.add(publicists)
+            if item['model'] == 'book':
+                books = Book(id=item['pk'], title=item['fields']['title'], id_publisher=item['fields']['id_publisher'])
+                session.add(books)
+            if item['model'] == 'shop':
+                shops = Shop(id=item['pk'], name=item['fields']['name'])
+                session.add(shops)
+            if item['model'] == 'stock':
+                stocks = Stock(id=item['pk'], id_shop=item['fields']['id_shop'], id_book=item['fields']['id_book'], count=item['fields']['count'])
+                session.add(stocks)
+            if item['model'] == 'sale':
+                sales = Sale(id=item['pk'], price=item['fields']['price'], date_sale=item['fields']['date_sale'], count=item['fields']['count'], id_stock=item['fields']['id_stock'])
+                session.add(sales)
+            session.commit()
+    session.close()
 
 
-session.close()
+if __name__ == '__main__':
+    # add_data("tests_data.json")
--- models.py
+++ models.py
@@ -7,27 +7,21 @@ class Publicist(Base):
     __tablename__ = 'publisher'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=255), nullable=False)
+    books = relationship("Book", back_populates="publicist")
 
 class Book(Base):
     __tablename__ = 'books'
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=255), nullable=False)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-    publicist = relationship(Publicist, backref="publisher")
+    publicist = relationship("Publicist", back_populates="books")
+    stock = relationship("Stock", back_populates="book")
 
 class Shop(Base):
     __tablename__ = 'shop'
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=255), nullable=False)
-
-class Stock(Base):
-    __tablename__ = 'stock'
-    id = sq.Column(sq.Integer, primary_key=True)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    count = sq.Column(sq.Integer, nullable=False)
-    id_book_child = relationship(Book, backref="books")
-    id_shop_child = relationship(Shop, backref="parent")
+    parent = relationship("Stock", back_populates="shop")
 
 class Sale(Base):
     __tablename__ = 'sale'
@@ -36,7 +30,18 @@ class Sale(Base):
     date_sale = sq.Column(sq.Date)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-    stock_relation = relationship(Stock, backref="stock")
+    stock = relationship("Stock", back_populates="stock_relation")
+
+class Stock(Base):
+    __tablename__ = 'stock'
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("books.id"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    book = relationship("Book", back_populates="stock")
+    shop = relationship("Shop", back_populates="parent")
+    stock_relation = relationship("Sale", back_populates="stock")
+
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
